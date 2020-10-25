0d933960833420ac6d1256969f404b8c07c632ab
anis_gitlab <anisurrahman.bs23@gmail.com>
2020-10-25 15:56:00 +0600

    addded

--- Featurea/Program.cs
+++ Featurea/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Featurea
 {
@@ -7,6 +9,12 @@ namespace Featurea
     {
         static void Main(string[] args)
         {
+            Func<int, int> squre = x => x * x;
+            Func<int, int, int> add = (x, y) => x + y;
+            Action<int> write = x => Console.WriteLine(x);
+
+            write(squre(add(2, 3)));
+
             var developers = new Employee[]
             {
                 new Employee { Id = 1, Name = "Anis" },
@@ -18,9 +26,17 @@ namespace Featurea
                 new Employee { Id = 3, Name = "Alex" }
             };
 
-            foreach (var person in sales)
+            var query = developers.Where(e => e.Name.Length == 4)
+                                   .OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length <= 10
+                         orderby developer.Name
+                         select developer;
+
+            foreach (var e in query2)
             {
-                Console.WriteLine(person.Name);
+                Console.WriteLine(e.Name);
             }
         }
     }
