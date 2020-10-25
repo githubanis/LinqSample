fc8380d584f0ed3978fbc1d1a4239ff72ff85c91
anis_gitlab <anisurrahman.bs23@gmail.com>
2020-10-25 23:59:20 +0600

    Added 2 project

---
+++ Cars/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cars = ProcessFile("fuel.csv");
+        }
+
+        private static List<Car> ProcessFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Queries/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var movies = new List<Movies>
+            {
+                new Movies{ Title = "The Dark Knight", Rating = 8.9f, Year = 2008 },
+                new Movies{ Title = "Star Wars V", Rating = 8.0f, Year = 1980 },
+                new Movies{ Title = "The King's Speech", Rating = 9.2f, Year = 2010 }
+            };
+
+            var query = movies.Where(y => y.Year > 2000);
+
+            var enumerator = query.GetEnumerator();
+            while(enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Title);
+            }
+        }
+    }
+}
